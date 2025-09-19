41ae4f28117b1407a3608aea170e1cc3585a6af7
Robert Vogel <vogel@hallowelt.biz>
2025-09-19 12:41:36 +0200

    Bump Spring Boot version

--- src/main/java/com/hallowelt/mediawiki/services/html2pdf/MainController.java
+++ src/main/java/com/hallowelt/mediawiki/services/html2pdf/MainController.java
@@ -59,7 +59,7 @@ public class MainController {
 		Map<String, Object> response = new HashMap<>();
 		response.put("success", true);
 		response.put("msg", "Service is running");
-		response.put( "version", "1.0.6");
+		response.put( "version", "1.0.7");
 		return response;
 	}
 
